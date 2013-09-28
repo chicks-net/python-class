7be13f6c8b2ce0c90d9a1af3a404b7972a3912e0
Christopher Hicks <chicks.net@gmail.com>
2013-09-28 15:36:56 -0700

    3 hegagons

--- turtle_funcs.py
+++ turtle_funcs.py
@@ -6,10 +6,23 @@ def line_without_moving():
     turtle.forward(50)
     turtle.backward(50)
 
-for _ in range(3):
-	line_without_moving()
-	turtle.right(90)
+def square():
+	for _ in range(4):
+		turtle.forward(90)
+		turtle.left(90)
 
-line_without_moving()
+def hexagon():
+	for _ in range(6):
+		turtle.forward(90)
+		turtle.left(60)
+
+def hexagon2():
+	for _ in range(3):
+		hexagon()
+		turtle.left(120)
+
+#square()
+hexagon2()
+
 
 turtle.exitonclick()
