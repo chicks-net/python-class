c4f9a46ae280ee425ff0af914d8976ebc0929de8
Christopher Hicks <chicks.net@gmail.com>
2013-09-28 14:54:37 -0700

    draw a square

---
+++ turtle_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import turtle
+
+turtle.shape("turtle")
+
+turtle.forward(25)
+turtle.left(90)
+
+turtle.forward(25)
+turtle.left(90)
+
+turtle.forward(25)
+turtle.left(90)
+
+turtle.forward(25)
+turtle.left(90)
+
+turtle.exitonclick()
