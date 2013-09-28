78f8c3e0fb8c1dc50b5f918ea929965d3d45744c
Christopher Hicks <chicks.net@gmail.com>
2013-09-28 15:32:47 -0700

    x marks the spot

--- turtle_funcs.py
+++ turtle_funcs.py
@@ -2,8 +2,14 @@
 
 import turtle
 
-turtle.forward(25)
-turtle.left(30)
-turtle.forward(35)
-turtle.shape("turtle")
+def line_without_moving():
+    turtle.forward(50)
+    turtle.backward(50)
+
+for _ in range(3):
+	line_without_moving()
+	turtle.right(90)
+
+line_without_moving()
+
 turtle.exitonclick()
