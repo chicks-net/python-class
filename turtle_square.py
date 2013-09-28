6cdb50009e9a50822cab4442175d29398e952047
Christopher Hicks <chicks.net@gmail.com>
2013-09-28 14:56:26 -0700

    colorful square

--- turtle_square.py
+++ turtle_square.py
@@ -7,12 +7,15 @@ turtle.shape("turtle")
 turtle.forward(25)
 turtle.left(90)
 
+turtle.color("red")
 turtle.forward(25)
 turtle.left(90)
 
+turtle.color("green")
 turtle.forward(25)
 turtle.left(90)
 
+turtle.color("blue")
 turtle.forward(25)
 turtle.left(90)
 
