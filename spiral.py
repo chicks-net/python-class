249a89bd612d6919867df80cd8b09ab7cb827f1f
Christopher Hicks <chicks.net@gmail.com>
2013-09-28 16:05:37 -0700

    happy spiral

---
+++ spiral.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import turtle
+
+line_size = 6
+angle = 45;
+
+while 1:
+	turtle.forward(line_size)
+	line_size += 3
+	turtle.left(angle)
+	turtle.width( int( line_size/8 ) )
+
+	if turtle.distance(0,0) > 200:
+		break
+	
+
+turtle.exitonclick()
